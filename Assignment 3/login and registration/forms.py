766888c3bab839eb5a009896fab8d9af1a675598
hmorkonda <46632746+hmorkonda@users.noreply.github.com>
2020-07-01 18:15:57 -0500

    login-registration-profilemodel
    Contains code for login form, registration form, partly client profile form and model

---
+++ Assignment 3/login and registration/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+class RegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = (
+                "username",
+                "password1", 
+                "password2", 
+
+            )
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        
+        if commit:
+            user.save()
+        return user
+
+class EditProfileForm(UserChangeForm):
+
+    class Meta:
+        model = UserProfile
+
+        fields = (
+            'Full_Name',
+            'Address1', 
+            'Address2', 
+            'City', 
+            'State'
+        )
+
+    def __init__(self, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+    # def save(self, commit=True):
+
+    #     #self.fields['Address2'].required = False
+
+    #     user = super(EditProfileForm, self).save(commit=False)
+        
+    #     if commit:
+    #         user.save()
+    #     return user
+
+
