a6faae8907fe02cd7be8c5415eaae09c4ddfdaa2
evelyn1398 <66398354+evelyn1398@users.noreply.github.com>
2020-07-03 01:12:06 -0500

    Update views.py

--- Assignment 3/FuelQuoteForm/fuelQuoteForm/views.py
+++ Assignment 3/FuelQuoteForm/fuelQuoteForm/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import userFuelForm
+from .forms import FuelQuoteForm
+
 
 # Create your views here.
 def fqf(request):
-    return render(request, 'fuelform.html')
+    if request.method == 'POST':
+        form = FuelQuoteForm(request.POST, instance=request.user.userFuelForm)
+
+        if form.is_valid():
+            form.save()
+            return redirect('fqf')
+
+    form = FuelQuoteForm(instance=request.user)
+    return render(request=request,
+                  template_name="fuelQuoteForm/fuelform.html",
+                  context={"form": form})
