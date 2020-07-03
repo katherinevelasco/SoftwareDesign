d8faa1288a19ef8ff71db7018564af8e4a6329bb
evelyn1398 <66398354+evelyn1398@users.noreply.github.com>
2020-07-03 01:11:29 -0500

    Update models.py

--- Assignment 3/FuelQuoteForm/fuelQuoteForm/models.py
+++ Assignment 3/FuelQuoteForm/fuelQuoteForm/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-
+from django.db.models.signals import post_save
 # Create your models here.
+
 class userFuelForm(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="related user")
     gallsRequested = models.IntegerField('Gallons Requested', default=0)
@@ -12,4 +12,8 @@ class userFuelForm(models.Model):
     total = models.DecimalField('Total (Price * Gallons)', decimal_places=2,
                                 max_digits=10000)  # total will be calculated by (suggPrice*gallsRequested)
 
-# class pricingModule(models.Model):
+    def create_quote(sender, **kwargs):
+        if kwargs['created']:
+           user_quote = userFuelForm.objects.create(user=kwargs['instance'])
+
+    post_save.connect(create_quote, sender= User)
