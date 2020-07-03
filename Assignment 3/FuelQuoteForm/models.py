67e09bd4929fa324d7ce06171e7a1d9aef9415aa
evelyn1398 <66398354+evelyn1398@users.noreply.github.com>
2020-07-02 20:45:48 -0500

    Update models.py

--- Assignment 3/FuelQuoteForm/models.py
+++ Assignment 3/FuelQuoteForm/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 class userFuelForm(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="related user")
-    gallsRequested = models.IntegerField(default=0)
-    deliveryAddress = models.CharField(default='', max_length=100)
-    deliveryDate = models.IntegerField(default=0)
-    suggPrice = models.IntegerField(default=0)
-   # total = models.IntegerField(default=gallsRequested*suggPrice)
+    gallsRequested = models.IntegerField('Gallons Requested', default=0)
+    deliveryAddress = models.TextField('Delivery Address')
+    deliveryDate = models.DateField(u'Delivery Date', help_text=u'Delivery Date')
+    suggPrice = models.IntegerField('Suggested Price',default=0)
+    total = models.IntegerField('Total (Price * Gallons)',default=0) #total will be calculated by (suggPrice*gallsRequested)
 
-class pricingModule(models.Model)
+#class pricingModule(models.Model):
