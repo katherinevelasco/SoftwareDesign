230b9f1fb6e558420ce112245062bc0cd5e14be6
hmorkonda <46632746+hmorkonda@users.noreply.github.com>
2020-07-03 19:23:04 -0500

    main urls.py

--- Assignment 3/login and registration/urls.py
+++ Assignment 3/login and registration/urls.py
@@ -1,18 +1,25 @@
+"""djangoProject URL Configuration
 
-from django.urls import path
-from . import views 
-
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name = 'accounts-home'),
-    #path('login/', views.loginView, name= 'mylogin'),
-    path('register/', views.register, name='register'),
-    path('logout/', views.logout_request, name='logout'),
-    path('login/', views.login_request, name= 'login'),
-    path('profile/', views.profile, name= 'profile'),
-    path('edit_profile/', views.edit_profile, name= 'edit_profile'),
-    path('fuelhistory/', views.fuelhistory, name= 'fuelhistory'),
-    path('fuelform/', views.fqf, name = 'fuelform'),
-
+    path('admin/', admin.site.urls),
+    #path('account/', include('accounts.urls'))
+    #path('account/', include('django.contrib.auth.urls')),
+    path('', include('accounts.urls'))
 
 ]
