b4447ed254771a6d6a893b2afc20fe8c1fc52c72
evelyn1398 <66398354+evelyn1398@users.noreply.github.com>
2020-07-04 13:38:58 -0500

    Update models.py

--- Assignment 3/FuelQuoteForm/fuelQuoteForm/models.py
+++ Assignment 3/FuelQuoteForm/fuelQuoteForm/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+
 # Create your models here.
 
 class userFuelForm(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="related user")
     gallsRequested = models.IntegerField('Gallons Requested', default=0)
-    deliveryAddress = models.TextField('Delivery Address')
-    deliveryDate = models.DateField(u'Delivery Date')
-    suggPrice = models.DecimalField('Suggested Price', decimal_places=2, max_digits=10000)
+    deliveryAddress = models.CharField('Delivery Address', max_length=100)
+    deliveryDate = models.DateTimeField()
+    suggPrice = models.IntegerField('Suggested Price', default=0)
     total = models.DecimalField('Total (Price * Gallons)', decimal_places=2,
-                                max_digits=10000)  # total will be calculated by (suggPrice*gallsRequested)
+                                max_digits=9, default=0.00000000 )  # total will be calculated by (suggPrice*gallsRequested)
 
     def create_quote(sender, **kwargs):
         if kwargs['created']:
-           user_quote = userFuelForm.objects.create(user=kwargs['instance'])
+            user_quote = userFuelForm.objects.create(user=kwargs['instance'])
 
     post_save.connect(create_quote, sender= User)
+
+
