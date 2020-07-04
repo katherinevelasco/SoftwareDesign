2b9f0a7d621ff73dcede30d14a4bddb202132d7a
evelyn1398 <66398354+evelyn1398@users.noreply.github.com>
2020-07-04 13:47:33 -0500

    Update models.py

--- Assignment 3/FuelQuoteForm/fuelQuoteForm/models.py
+++ Assignment 3/FuelQuoteForm/fuelQuoteForm/models.py
@@ -8,7 +8,7 @@ class userFuelForm(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="related user")
     gallsRequested = models.IntegerField('Gallons Requested', default=0)
     deliveryAddress = models.CharField('Delivery Address', max_length=100)
-    deliveryDate = models.DateTimeField()
+    deliveryDate = models.DateTimeField('Delivery Date')
     suggPrice = models.IntegerField('Suggested Price', default=0)
     total = models.DecimalField('Total (Price * Gallons)', decimal_places=2,
                                 max_digits=9, default=0.00000000 )  # total will be calculated by (suggPrice*gallsRequested)
