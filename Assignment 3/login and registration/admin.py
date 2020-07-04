d3447c8c7ce81598300e7186c345152df1fa0a9a
hmorkonda <46632746+hmorkonda@users.noreply.github.com>
2020-07-03 19:18:04 -0500

    Update admin.py

--- Assignment 3/login and registration/admin.py
+++ Assignment 3/login and registration/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from accounts.models import UserProfile
+from accounts.models import UserProfile, PricingModule, UserFuelForm
+
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(UserFuelForm)
+admin.site.register(PricingModule)
