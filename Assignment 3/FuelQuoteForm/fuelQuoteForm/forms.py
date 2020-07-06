e8336a961ae4267cc62ad6886b3dfc7e891525b3
evelyn1398 <66398354+evelyn1398@users.noreply.github.com>
2020-07-05 22:43:28 -0500

    Update forms.py

--- Assignment 3/FuelQuoteForm/fuelQuoteForm/forms.py
+++ Assignment 3/FuelQuoteForm/fuelQuoteForm/forms.py
@@ -1,17 +1,24 @@
 from django import forms
-from .models import userFuelForm
+from .models import UserFuelForm
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class FuelQuoteForm(forms.ModelForm):
     class Meta:
-        model = userFuelForm
+        model = UserFuelForm
         fields = (
             "gallsRequested",
             "deliveryAddress",
             "deliveryDate",
             "suggPrice",
             "total",
+            "user",
         )
-
+        widgets = {
+            "deliveryDate": DateInput(format=('%d-%m-%Y'), attrs={'firstDay':1, 'pattern=': '\d{4}-\d{2}-\d{2}',
+                                                               'lang': 'pl', 'format': 'yyyy-mm-dd', 'type': 'date'}),
+      }
     def save(self, commit=True):
         user = super(FuelQuoteForm, self).save(commit=False)
 
