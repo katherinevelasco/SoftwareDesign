b05fa2c72cac7606595085aec14f01935cf92d13
hmorkonda <46632746+hmorkonda@users.noreply.github.com>
2020-07-09 23:38:59 -0500

    Create admin.py

---
+++ Assignment 4/accounts/admin.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from accounts.models import UserProfile, PricingModule, UserFuelForm
+
+# Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(UserFuelForm)
+
+admin.site.register(PricingModule)
